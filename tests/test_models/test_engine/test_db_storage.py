b04f2595c7a674b2cad9662b991b4a56013ee584
Saysherief <suhairdama@gmail.com>
2023-09-30 14:45:38 -0700

    some modifications

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -70,7 +70,9 @@ test_db_storage.py'])
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    HBNB_ENV = os.getenv('HBNB_TYPE_STORAGE')
+
+    @unittest.skipIf(HBNB_ENV != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
